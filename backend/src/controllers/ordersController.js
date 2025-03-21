16295224921d842843c1cd7297d1d383ab739efc
Pablo Menjívar <134906528+pablo-menjivar@users.noreply.github.com>
2025-03-21 09:28:58 -0600

    crud 1

---
+++ backend/src/controllers/ordersController.js
@@ -0,0 +1,77 @@
+import ordersModel from "../models/Orders.js"; 
+
+const ordersController = {};
+//Create (Post)
+ordersController.postOrders = async (req, res) => {
+    const {customerId, items, total, status} = req.body;
+    //Validacion para que los items (productos) contengan los campos necesarios
+    try {
+        if (!items || !Array.isArray(items) || items.length === 0) {
+            return res.status(400).json({ message: "El arreglado de los productos es necesario y no puede ser vacio." });
+        }
+    
+    const newOrder = new ordersModel({customerId, items, total, status});
+
+    await newOrder.save();
+    res.status(201).json({ message: "Order creada exitosamente." });
+    } catch (error) {
+        res.status(400).json({ message: "Error al crear la orden", error: error.message });
+    };
+};
+//Read (Get)
+ordersController.getOrders = async (req, res) => {
+    try {
+        const orders = await ordersModel.find()
+        .populate("customersId", "username email") 
+        .populate({path: "items.itemId", select: "name price", }); 
+        res.status(200).json(orders);
+    } catch (error) {
+        res.status(500).json({ message: "Error al obtener ordenes", error: error.message });
+    };
+};
+//Update (Put)
+ordersController.putOrders = async (req, res) => {
+    const {customerId, items, total, status} = req.body;
+    try {
+        const updates = {customerId, items, total, status};
+
+        const updatedOrder = await ordersModel.findByIdAndUpdate(req.params.id, updates, { new: true })
+            .populate("cuestomersId", "username email")
+            .populate({path: "items.itemId", select: "name price",}); 
+        if (!updatedOrder) {
+            return res.status(404).json({ message: "Orden no encontrada" });
+        }
+        res.status(200).json({ message: "Orden actualizada correctamente"});
+    } catch (error) {
+        res.status(400).json({ message: "Error actualizando la orden", error: error.message });
+    }
+};
+// Delete (Delete) por su ID
+ordersController.deleteOrders = async (req, res) => {
+    try {
+      const deletedOrder = await ordersModel.findByIdAndDelete(req.params.id);
+  
+      if (!deletedOrder) {
+        return res.status(404).json({ message: "Order no encontrada" });
+      }
+  
+      res.status(200).json({ message: "Orden elimanada correctamente" });
+    } catch (error) {
+      res.status(400).json({ message: "Error al eliminar la orden", error: error.message });
+    }
+  };
+//Read (Get) pero por su ID
+ordersController.getOrder = async (req, res) => {
+    try {
+        const order = await ordersModel.find(req.params.id)
+        .populate("customersId", "username email") 
+        .populate({path: "items.itemId", select: "name price", }); 
+        if (!order) {
+            return res.status(404).json({ message: "Orden no encontrada" });
+        }
+        res.status(200).json(order);
+    } catch (error) {
+        res.status(500).json({ message: "Error al obtener ordenes", error: error.message });
+    };
+};
+export default ordersController;
--- backend/src/models/orders.js
+++ backend/src/models/orders.js
@@ -2,64 +2,48 @@
 Se verá algo así, ya que incluye ambas tablas de Artículos y Piezas de Arte
 "Orders": [
     {
-        "customerId": "5f5b7b3b4f3b1b2b3c4f5d6e",
-        "items": [
+  "customerId": "5f5b7b3b4f3b1b2b3c4f5d6e",
+  "items": [
         {
-            "articlesId": "5f5b7b3b4f3b1b2b3c4f5d6e",
+            "itemType": "Article",
+            "itemId": "5f5b7b3b4f3b1b2b3c4f5d6e",
             "quantity": 2,
             "subtotal": 100
         },
         {
-            "articlesId": "5fbf7b3b4f3b1b2bsbd4f5d6e",
+            "itemType": "Article",
+            "itemId": "5fbf7b3b4f3b1b2bsbd4f5d6e",
             "quantity": 1,
             "subtotal": 50
         },
         {
-            "articlesId": "5f5b7b3b4f3b1b2b3c4f5d6e",
-            "quantity": 3,
-            "subtotal": 150
-        },
-        {
-            "artPiecesId": "67afad03e0548fb3da69ecb3",
+            "itemType": "ArtPiece",
+            "itemId": "67afad03e0548fb3da69ecb3",
             "quantity": 1,
             "subtotal": 400
-        },
-        {
-            "artPiecesId": "67afad03e0548fb3da69ecb4",
-            "quantity": 1,
-            "subtotal": 600
-        },
-        {
-            "artPiecesId": "67afad03e0548fb3da69ecb5",
-            "quantity": 3,
-            "subtotal": 1800
-        },
-        ]
-        "total": 3100,
-        "status": "Pending"
-    }
-] */
-// Importamos Schema y model de mongoose
-import { Schema, model } from "mongoose";
-// Creamos un nuevo Schema con la estructura de los productos
-const ordersSchema = new Schema(
-  {
-    customerId: { type: Schema.Types.ObjectId, require: true,},
-    items: [
-    {
-        articlesId: { type: Schema.Types.ObjectId, ref: "Article", require: false, },
-        artPiecesId: { type:  Schema.Types.ObjectId, ref: "ArtPiece", require: false, },
-        quantity: { type: Number, require: true, require: true, min: 1, },
-        subtotal: { type: Number, require: true, require: true, min: 0, },
-    },
+        }
     ],
-    total: { type: Number, required: true, min: 0, },
-  },
-  {  
-    timestamps: true,
-    strict: false,
-  }
-);
-// NUNCA OLVIDAR: colocar el tercer argumento para que se cree la colección con el nombre que queremos,
-// ya que por defecto mongoose pluraliza el nombre de la colección
+    "total": 550,
+    "status": "Pending"
+    },
+] */
+import { Schema, model } from "mongoose";
+
+const orderItemSchema = new Schema({
+    itemType: { type: String, required: true, enum: ['Article', 'ArtPiece'] },
+    itemId: { type: Schema.Types.ObjectId, required: true, refPath: 'itemType'},
+    quantity: { type: Number, required: true, min: 1 },
+    subtotal: { type: Number, required: true, min: 0 }
+});
+const ordersSchema = new Schema({
+    customerId: { type: Schema.Types.ObjectId, required: true,ref: "Customer" },
+    items: [orderItemSchema],
+    total: { type: Number, required: true, min: 0 },
+    status: { type: String, required: true, enum: ['Pending', 'Processing', 'Shipped', 'Delivered', 'Cancelled'], default: 'Pending'}
+    },
+    {  
+      timestamps: true,
+      strict: false
+    }
+  );
 export default model("Order", ordersSchema, "Order");

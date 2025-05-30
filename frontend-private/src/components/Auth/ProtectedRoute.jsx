a53d4363a48c4ac3b8316444e6362b828ecda9b3
Pablo Menjivar <20200135@ricaldone.edu.svgit config --global user.email 20200135@ricaldone.edu.sv>
2025-05-30 11:03:18 -0600

    xd

--- frontend-private/src/components/Auth/ProtectedRoute.jsx
+++ frontend-private/src/components/Auth/ProtectedRoute.jsx
@@ -1,4 +1,3 @@
-// src/components/Auth/ProtectedRoute.jsx
 import { useEffect, useState } from 'react'
 import { Navigate, useLocation } from 'react-router-dom'
 import { useAuth } from '../../hooks/useAuth'
